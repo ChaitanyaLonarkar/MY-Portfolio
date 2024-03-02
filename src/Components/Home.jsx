9df009787ce13ecd4f1ba60cc0b3c032de0cf55c
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-02 22:04:43 +0530

    added animation ss

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,16 +9,19 @@ export default function Home() {
         <div className="left">
           <h3>CHAITANYA LONARKAR</h3>
           <h1>Frontend Devloper</h1>
-          <h1 className="f">-FullStack Devloper</h1>
+          <div className="f">
+            <h1 className="ff">-FullStack Devloper</h1>
+            <div className="fff">
+              
+            </div>
+          </div>
           {/* <h1 className="b">-Backend Devloper</h1>
           <h1 className="a">-Android Devloper</h1> */}
         </div>
         <div className="left-img">
           <img src={myimg} alt="Chaitanya" />
         </div>
-        
       </section>
-      
     </>
   );
 }
