a3acebcc871db6f2da9b12bff8843dba260d0093
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-03 14:58:23 +0530

    sbtns

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import myimg from "../assets/img/imgCcc.jpg";
 import myimg2 from "../assets/img/bimg.jpg";
-
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaSquareTwitter } from "react-icons/fa6";
 export default function Home() {
   return (
     <>
@@ -11,10 +13,25 @@ export default function Home() {
           <h1>Frontend Devloper</h1>
           <div className="f">
             <h1 className="ff">-FullStack Devloper</h1>
-            <div className="fff">
-              
+            <div className="fff"></div>
+          </div>
+          <div className="kbtn">
+            {/* <button>Know Me</button> */}
+            <button>Who I am?</button>
+            <div className="slinks">
+              <a href="http://" target="_blank">
+                <FaLinkedin />
+              </a>
+              <a href="http://" target="_blank">
+                <FaSquareGithub />
+              </a>
+              <a href="http://" target="_blank">
+                <FaSquareTwitter />
+              </a>
+              {/* <a href="http://" target="_blank"></a> */}
             </div>
           </div>
+
           {/* <h1 className="b">-Backend Devloper</h1>
           <h1 className="a">-Android Devloper</h1> */}
         </div>
