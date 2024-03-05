a71c517036395aa16f8d3b263a329756d52cff70
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-05 23:48:38 +0530

    kuch nhi hua

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,6 +1,21 @@
 import React from "react";
-
+import { useEffect } from "react";
 export default function Project() {
+//   const element = document.getElementById("projects");
+//   console.log("ndfjnsjdnfjsnd",element)
+//   // console.log(scrollContainer)
+//   const wheel =( (event) => {
+//     event.preventDefault();
+  
+//     element.scrollBy({
+//       left: event.deltaY < 0 ? -30 : 30,
+      
+//     });
+//   useEffect(() => {
+//     wheel();
+//   }, [])
+// });
+
   return (
     <>
     <section>
@@ -11,7 +26,7 @@ export default function Project() {
                 I have a play a key role in developing impactful projects. Here are curated selection showcasing my expertise and achieved results.
             </p>
         </div>
-        <div className="prights">
+        <div className="prights" onWheel={wheel}>
              <div className="project">netflix clone</div>  
              <div className="project">Animated wesites</div>
              <div className="project">Animated wesites</div>
