98714a864c4fecdfd42138e7ddf71c039a5b6136
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-06 16:26:22 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   
   return (
     <>
-      <Animation />
       <div className="sectionHome">
+      <Animation />
         <Navbar />
         <Home />
       </div>
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -20,12 +20,12 @@ export default function Project() {
     <>
     <section>
       <div id="projects" className="projects">
-        <div className="pleft">
+        {/* <div className="pleft">
             <h2>Selected Works</h2>
             <p>
                 I have a play a key role in developing impactful projects. Here are curated selection showcasing my expertise and achieved results.
             </p>
-        </div>
+        </div> */}
         <div className="prights" >
              <div className="project">netflix clone</div>  
              <div className="project">Animated wesites</div>
