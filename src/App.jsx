619ac76bbabd649261549004c67a2b22e272acf0
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-14 02:02:09 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,32 @@ import Project from "./Components/Project";
 import { useParallax } from "react-scroll-parallax";
 import Skills from "./Components/Skills";
 import Preloader from "./Components/Preloader";
+import Partiles from "./Components/Partiles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const parallax = useParallax({
     // rotate: [0, 360],
-    scale: [ 1.1,0.7, "easeInQuad"],
-    display: ["flex","none"]
+    scale: [1.1, 0.7, "easeInQuad"],
+    display: ["flex", "none"],
     // translateY: [ 0,20 ],
     // display: "none"
-
   });
-  
-  return (
-    <><Preloader/>
-          <Navbar />
-        <div className="sectionHome" ref={parallax.ref}>
 
+  return (
+    <>
+      <Preloader />
+      <Navbar />
+      <div className="sectionHome" ref={parallax.ref}>
         {/* <div className="sectionHome" > */}
-          <Animation />
-          <Home />
-        </div>
-      {/* <marquee onmouseover="this.stop();" onmouseout="this.start();">jhdhfjasdfhasdjkfhjkashdjfhjashdjfhjsahdhfjkshdjkfhjsahdjfhjasdf</marquee> */}
-      <Project />
-      <Skills/>
+        <Animation />
+        <Home />
+      </div>
+      <div className="Allsection">
+        {/* <Partiles/> */}
+        <Project />
+        <Skills />
+      </div>
     </>
   );
 }
