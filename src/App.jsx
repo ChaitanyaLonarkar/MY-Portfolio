4d97afbee54234884792cf6ef35b1c7e5ba7842c
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-16 21:00:21 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Navbar from "../src/Components/Navbar";
 import Home from "./Components/Home";
@@ -8,39 +7,34 @@ import About from "./Components/About";
 
 import Animation from "./Components/Animation";
 import Project from "./Components/Project";
-import { useParallax } from "react-scroll-parallax";
 import Skills from "./Components/Skills";
 import Preloader from "./Components/Preloader";
 import Partiles from "./Components/Partiles";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const parallax = useParallax({
-    // rotate: [0, 360],
-    scale: [1.1, 0.7, "easeInQuad"],
-    
-    // translateY: [ 0,20 ],
-    // display: ["flex","none","easeInQuad"]
-  });
+  
 
-  // const [hideHomePage, setHideHomePage] = useState(false);
+  const [hideHomePage, setHideHomePage] = useState(false);
 
   // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (window.scrollY >= window.innerHeight) {
-  //       // setHideHomePage(true);
-  //       const home=document.getElementById("sectionHome")
-  //       home.style.display="hidden"
-  //     } else {
-  //       setHideHomePage(false);
-  //     }
-  //   };
+    const handleScroll = () => {
+      if (window.scrollY >= window.innerHeight) {
+        setHideHomePage(true);
+        // console.log("chaitanya")
+        // const home=document.getElementById("sectionHome")
+        // home.style.display="hidden"
+      } else {
+        setHideHomePage(false);
+        // console.log("no")
 
-  //   window.addEventListener('scroll', handleScroll);
+      }
+    };
 
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
+    window.addEventListener('scroll', handleScroll);
+
+    // return () => {
+    //   window.removeEventListener('scroll', handleScroll);
+    // };
   // }, []);
 
 
@@ -48,10 +42,10 @@ function App() {
     <>
       <Preloader />
       <Navbar />
-      <div id="sectionHome" ref={parallax.ref}>
-        {/* <div className="sectionHome" > */}
         <Animation />
-        <Home />
+      <div id="sectionHome" >
+        {setHideHomePage?<Home />:""}
+        
       </div>
       <div className="Allsection">
         {/* <Partiles/> */}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,13 +4,25 @@ import myimg2 from "../assets/img/bimg.jpg";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareGithub } from "react-icons/fa6";
 import { FaSquareTwitter } from "react-icons/fa6";
+import { useParallax } from "react-scroll-parallax";
+import { useState, useEffect } from "react";
+
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const parallax = useParallax({
+    // rotate: [0, 360],
+    scale: [1.1, 0.7, "easeInQuad"],
+    
+    // translateY: [ 0,20 ],
+    // display: ["flex","none","easeInQuad"]
+  });
+
   
 
   return (
     <>
-      <section className="home" >
+      <section className="home" ref={parallax.ref} >
          <div className="left-img">
           <img src={myimg} alt="Chaitanya" />
         </div>
