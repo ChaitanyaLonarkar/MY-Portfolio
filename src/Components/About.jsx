384bc19d5e7c5243b6f1ac7f4288582045f9ff25
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-17 01:11:16 +0530

    about modify

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,37 +1,51 @@
 import React from "react";
+import { HiOutlineDocumentArrowDown } from "react-icons/hi2";
 
 export default function About() {
   return (
     <>
+    <div className="about-head">
+      <p>About-</p>
+      <p>-Education</p>
+    </div>
       <div className="about-edu">
         <div className="about">
-          <h1>About</h1>
+          <h1>About Me -</h1>
           <p>
             With 10 years experience as a professional Web developer, I have
             acquired the skills and knowledge necessary to make your project a
             success. I enjoy every step of the design process, from discussion
             and collaboration.
           </p>
-          <button>Resume</button>
+          <a href="http://" target="_blank" rel="noopener noreferrer">Download CV<HiOutlineDocumentArrowDown /></a>
+          
         </div>
         <div className="education">
-          <h1>Educations</h1>
+          <h1>Educations -</h1>
           <div className="edu">
-            <h2>Computer science engineering</h2>
+            <div className="edu-year">
+              <h2>Computer science engineering</h2>
+              <p>2022-2025</p>
+            </div>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
           <div className="edu">
-            <h2>Computer science engineering</h2>
+            <div className="edu-year">
+              <h2>Computer engineering</h2>
+              <p>2020-2022</p>
+
+            </div>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
           <div className="edu">
-            <h2>Computer science engineering</h2>
+            <div className="edu-year">
+              <h2>HSC</h2>
+              <p>2018-2020</p>
+
+            </div>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
-          <div className="edu">
-            <h2>Computer science engineering</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </div>
+          
         </div>
       </div>
     </>
