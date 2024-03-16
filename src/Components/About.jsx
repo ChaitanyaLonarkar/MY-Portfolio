0d7e9d5a0588799f7f4af1530063e4870115e461
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-16 23:59:31 +0530

    ad

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,12 +3,36 @@ import React from "react";
 export default function About() {
   return (
     <>
-      <div className="about">
-        <h2>About</h2>
-        <p>With 10 years experience as a professional Web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration.</p>
-      </div>
-      <div className="education">
-
+      <div className="about-edu">
+        <div className="about">
+          <h1>About</h1>
+          <p>
+            With 10 years experience as a professional Web developer, I have
+            acquired the skills and knowledge necessary to make your project a
+            success. I enjoy every step of the design process, from discussion
+            and collaboration.
+          </p>
+          <button>Resume</button>
+        </div>
+        <div className="education">
+          <h1>Educations</h1>
+          <div className="edu">
+            <h2>Computer science engineering</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="edu">
+            <h2>Computer science engineering</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="edu">
+            <h2>Computer science engineering</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="edu">
+            <h2>Computer science engineering</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
       </div>
     </>
   );
